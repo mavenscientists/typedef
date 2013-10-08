bb05fe93ef7b0d3f21ea37fd44df966d7f4e5d5c
Karan <karan4c6@gmail.com>
2013-10-08 16:17:13 +0530

    typedef

---
+++ typedef.c
@@ -0,0 +1,18 @@
+/**
+   * @author    Maven Scientists (http://mavenscientists.com) 
+   * @email    info@mavenscientists.com
+   * Array - this program shows different methods, how to use arrays in C
+   */
+
+#include <stdio.h>
+
+typedef int I;
+	
+int main()
+{
+	//you can use I instead of writing int
+	I number  = 10;
+	printf("%d", number);
+	
+	return 0;
+}
